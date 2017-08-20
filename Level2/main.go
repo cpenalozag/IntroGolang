04b3d59742e21c7b3a487117551ea0f186330877
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-08-20 18:26:17 -0500

    Time func

--- Level2/main.go
+++ Level2/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-  "chat/util"
-  "os"
+	"os"
+	"time"
+	"util"
 )
 
 func main() {
-  listenPort:="8080"
-  args := os.Args
-  if len(args) > 1 {
-    hostIP:=args[1]
-    util.RunGuest(hostIP)
-  } else {
-    listenIP:=util.GetLocalNetworkIP()
-    util.RunHost(listenIP + ":" + listenPort)
-  }
+	listenPort := "8080"
+	args := os.Args
+	if len(args) > 1 {
+		hostIP := args[1]
+		util.RunGuest(hostIP)
+	} else {
+		listenIP := util.GetLocalNetworkIP()
+		util.RunHost(listenIP + ":" + listenPort)
+	}
+}
+
+func getListenPort() string {
+	hourOfDay := time.Now().Hour()
+	if hourOfDay < 12 {
+		return "8080"
+	} else if hourOfDay < 20 {
+		return "8081"
+	} else {
+		return "8082"
+	}
 }
