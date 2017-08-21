704c79f74cd7a2c6a7e35061c653ff9fd7f9ed41
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-08-20 23:24:46 -0500

    Level 4

--- Level4/main.go
+++ Level4/main.go
@@ -2,32 +2,39 @@ package main
 
 import (
   "fmt"
-  "chat/util"
 )
 
 type guestConnection struct {
   ip string
   userName string
+  isAdmin bool
 }
 
-func ( g guestConnection ) isAllowed() bool {
+func (g guestConnection) isAllowed() bool {
   return !isIPBlocked(g.ip) && g.userName != "Darth Vader"
 }
 
 func main() {
   ip := util.GetGuestIP()
-  userName := "Kerry"
+  userName := "Obi-Wan"
 
-  gConn := guestConnection{ip: ip, userName: userName}
-  isAllowedStatus := gConn.isAllowed()
-  fmt.Println(isAllowedStatus)
+  gConn := &guestConnection{ip: ip, userName: userName}
+  fmt.Println("Before auth", gConn)
+  authorizeAdmin(gConn)
+  fmt.Println("After auth", gConn)
+}
+
+func authorizeAdmin( c * guestConnection){
+  if c.isAllowed() && c.ip == "192.168.0.10" {
+    c.isAdmin = true
+  }
 }
 
 func isIPBlocked(ip string) bool {
   blockedIPs := getBlockedIPs()
   for _, blockedIP := range blockedIPs {
     if blockedIP == ip {
-      return true
+        return true
     }
   }
   return false
@@ -35,4 +42,9 @@ func isIPBlocked(ip string) bool {
 
 func getBlockedIPs() []string {
   return []string{"192.168.0.17", "192.168.0.18", "192.168.0.19", "192.168.0.20"}
+}
+
+func GetGuestIP() string{
+  // returns guest IP
+  return "192.168.1.2"
 }
