c4624e00eef0989afa95621bc0622422c96b3d5e
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-08-20 23:50:51 -0500

    Packages

--- Level5/main.go
+++ Level5/main.go
@@ -1,46 +1,12 @@
 package main
 
-import (
-  "fmt"
-  "time"
+import(
+  "Level5/lib"
 )
 
-type guestConnection struct {
-  ip       string
-  userName string
-  isAdmin  bool
-}
-
-type visitorConnection struct {
-  ip       string
-  connHour int
-}
-type notifier interface {
-  notify()
-}
-
-
-// one implementation for guestConnection
-func (g guestConnection) notify() {
-  fmt.Println("Guest connection from user name:", g.userName)
-}
-
-// and a different implementation for visitorConnection
-func (v visitorConnection ) notify() {
-  fmt.Println("Visitor connected at:", v.connHour)
-}
-
 func main() {
-  notifiers := getAllConnections()
+  notifiers := lib.GetAllConnections()
   for _, c := range notifiers {
-    c.notify()
+    c.Notify()
   }
-
-}
-
-func getAllConnections() []notifier {
-  gConn := &guestConnection{ip: "192.168.0.10", userName: "Darth Vader"}
-  vConn := &visitorConnection{ip: "192.168.0.11", connHour: time.Now().Hour()}
-
-  return []notifier{gConn, vConn}
 }
